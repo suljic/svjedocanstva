93b25fd87d67ee91a2f72967e088014c93f9094c
suljic <adnan.suljic.2016@gmail.com>
2019-02-22 21:04:09 +0100

    login

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.jMetroTextBoxEmail = new JMetroTextBox.JMetroTextBox();
+            this.jMetroTextBoxPassword = new JMetroTextBox.JMetroTextBox();
+            this.jThinButtonPrijaviMe = new JThinButton.JThinButton();
+            this.PictureBoxLogo = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonX = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // jMetroTextBoxEmail
+            // 
+            this.jMetroTextBoxEmail.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.jMetroTextBoxEmail.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.jMetroTextBoxEmail.BackColor = System.Drawing.Color.Transparent;
+            this.jMetroTextBoxEmail.BorderColor = System.Drawing.Color.Gray;
+            this.jMetroTextBoxEmail.BorderRadius = 12;
+            this.jMetroTextBoxEmail.FillColor = System.Drawing.Color.White;
+            this.jMetroTextBoxEmail.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.jMetroTextBoxEmail.Font_Size = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.jMetroTextBoxEmail.ForeColors = System.Drawing.Color.Gray;
+            this.jMetroTextBoxEmail.IsPassword = false;
+            this.jMetroTextBoxEmail.LineThickness = 2;
+            this.jMetroTextBoxEmail.Location = new System.Drawing.Point(261, 282);
+            this.jMetroTextBoxEmail.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.jMetroTextBoxEmail.MaxLength = 32767;
+            this.jMetroTextBoxEmail.MouseOnHover = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(131)))), ((int)(((byte)(213)))));
+            this.jMetroTextBoxEmail.Name = "jMetroTextBoxEmail";
+            this.jMetroTextBoxEmail.OnCursor = System.Windows.Forms.Cursors.IBeam;
+            this.jMetroTextBoxEmail.OnFocusColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(131)))), ((int)(((byte)(213)))));
+            this.jMetroTextBoxEmail.OnFocusFontColor = System.Drawing.Color.Empty;
+            this.jMetroTextBoxEmail.ReadOnly = false;
+            this.jMetroTextBoxEmail.Size = new System.Drawing.Size(273, 39);
+            this.jMetroTextBoxEmail.TabIndex = 0;
+            this.jMetroTextBoxEmail.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.jMetroTextBoxEmail.TextName = "";
+            this.jMetroTextBoxEmail.Load += new System.EventHandler(this.jMetroTextBoxEmail_Load);
+            // 
+            // jMetroTextBoxPassword
+            // 
+            this.jMetroTextBoxPassword.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.jMetroTextBoxPassword.BackColor = System.Drawing.Color.Transparent;
+            this.jMetroTextBoxPassword.BorderColor = System.Drawing.Color.Gray;
+            this.jMetroTextBoxPassword.BorderRadius = 12;
+            this.jMetroTextBoxPassword.FillColor = System.Drawing.Color.White;
+            this.jMetroTextBoxPassword.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.jMetroTextBoxPassword.Font_Size = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.jMetroTextBoxPassword.ForeColors = System.Drawing.Color.Gray;
+            this.jMetroTextBoxPassword.IsPassword = true;
+            this.jMetroTextBoxPassword.LineThickness = 2;
+            this.jMetroTextBoxPassword.Location = new System.Drawing.Point(261, 350);
+            this.jMetroTextBoxPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.jMetroTextBoxPassword.MaxLength = 32767;
+            this.jMetroTextBoxPassword.MouseOnHover = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(131)))), ((int)(((byte)(213)))));
+            this.jMetroTextBoxPassword.Name = "jMetroTextBoxPassword";
+            this.jMetroTextBoxPassword.OnCursor = System.Windows.Forms.Cursors.IBeam;
+            this.jMetroTextBoxPassword.OnFocusColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(131)))), ((int)(((byte)(213)))));
+            this.jMetroTextBoxPassword.OnFocusFontColor = System.Drawing.Color.Empty;
+            this.jMetroTextBoxPassword.ReadOnly = false;
+            this.jMetroTextBoxPassword.Size = new System.Drawing.Size(273, 39);
+            this.jMetroTextBoxPassword.TabIndex = 1;
+            this.jMetroTextBoxPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.jMetroTextBoxPassword.TextName = "";
+            // 
+            // jThinButtonPrijaviMe
+            // 
+            this.jThinButtonPrijaviMe.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.jThinButtonPrijaviMe.BackColor = System.Drawing.Color.Transparent;
+            this.jThinButtonPrijaviMe.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(131)))), ((int)(((byte)(213)))));
+            this.jThinButtonPrijaviMe.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(131)))), ((int)(((byte)(213)))));
+            this.jThinButtonPrijaviMe.BorderRadius = 20;
+            this.jThinButtonPrijaviMe.ButtonText = "PRIJAVI SE";
+            this.jThinButtonPrijaviMe.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.jThinButtonPrijaviMe.Font_Size = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.jThinButtonPrijaviMe.ForeColors = System.Drawing.Color.White;
+            this.jThinButtonPrijaviMe.HoverBackground = System.Drawing.Color.White;
+            this.jThinButtonPrijaviMe.HoverBorder = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(131)))), ((int)(((byte)(213)))));
+            this.jThinButtonPrijaviMe.HoverFontColor = System.Drawing.SystemColors.Highlight;
+            this.jThinButtonPrijaviMe.LineThickness = 2;
+            this.jThinButtonPrijaviMe.Location = new System.Drawing.Point(312, 412);
+            this.jThinButtonPrijaviMe.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.jThinButtonPrijaviMe.Name = "jThinButtonPrijaviMe";
+            this.jThinButtonPrijaviMe.Size = new System.Drawing.Size(168, 63);
+            this.jThinButtonPrijaviMe.TabIndex = 3;
+            // 
+            // PictureBoxLogo
+            // 
+            this.PictureBoxLogo.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.PictureBoxLogo.BackColor = System.Drawing.Color.White;
+            this.PictureBoxLogo.Image = ((System.Drawing.Image)(resources.GetObject("PictureBoxLogo.Image")));
+            this.PictureBoxLogo.Location = new System.Drawing.Point(312, 90);
+            this.PictureBoxLogo.Name = "PictureBoxLogo";
+            this.PictureBoxLogo.Size = new System.Drawing.Size(168, 147);
+            this.PictureBoxLogo.TabIndex = 4;
+            this.PictureBoxLogo.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gray;
+            this.label1.Location = new System.Drawing.Point(270, 266);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "E-mail";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Gray;
+            this.label2.Location = new System.Drawing.Point(270, 334);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Password";
+            // 
+            // buttonX
+            // 
+            this.buttonX.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonX.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(131)))), ((int)(((byte)(213)))));
+            this.buttonX.FlatAppearance.MouseDownBackColor = System.Drawing.Color.SkyBlue;
+            this.buttonX.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SkyBlue;
+            this.buttonX.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonX.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonX.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(131)))), ((int)(((byte)(213)))));
+            this.buttonX.Location = new System.Drawing.Point(768, 12);
+            this.buttonX.Name = "buttonX";
+            this.buttonX.Size = new System.Drawing.Size(20, 20);
+            this.buttonX.TabIndex = 8;
+            this.buttonX.Text = "X";
+            this.buttonX.UseVisualStyleBackColor = true;
+            this.buttonX.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.buttonX);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PictureBoxLogo);
+            this.Controls.Add(this.jThinButtonPrijaviMe);
+            this.Controls.Add(this.jMetroTextBoxPassword);
+            this.Controls.Add(this.jMetroTextBoxEmail);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MinimumSize = new System.Drawing.Size(700, 500);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private JMetroTextBox.JMetroTextBox jMetroTextBoxEmail;
+        private JMetroTextBox.JMetroTextBox jMetroTextBoxPassword;
+        private JThinButton.JThinButton jThinButtonPrijaviMe;
+        private System.Windows.Forms.PictureBox PictureBoxLogo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonX;
+    }
+}
+
